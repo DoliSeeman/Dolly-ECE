a2c0c42b19cdcb47faab68709771bea6a99cbce5
DoliSeeman <161722567+DoliSeeman@users.noreply.github.com>
2024-04-02 11:33:03 -0700

    Add files via upload

---
+++ pt4.js
@@ -0,0 +1,12 @@
+let num1 = prompt("Enter number A: ");
+let num2 = prompt("Enter number B: ");
+
+if(num1 > num2){
+    document.write("Number A is greater");
+}
+if(num1 < num2){
+    document.write("Number B is greater");
+}
+if(num1 === num2){
+    document.write("Both are equal");
+}
---
+++ pt5.js
@@ -0,0 +1,12 @@
+let num1 = prompt("Enter number A: ");
+let num2 = prompt("Enter number B: ");
+
+if(num1 > num2){
+    alert("Number A is greater");
+}
+if(num1 < num2){
+    alert("Number B is greater");
+}
+if(num1 === num2){
+    alert("Both are equal");
+}
